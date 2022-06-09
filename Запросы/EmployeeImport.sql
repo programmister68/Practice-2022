89de6223f243b5c80b608612d6c3bde682799389
s1creet <71034214+s1creet@users.noreply.github.com>
2022-06-09 21:36:57 +0300

    Add files via upload

---
+++ Запросы/EmployeeImport.sql
@@ -0,0 +1,16 @@
+/*
+-- Query: SELECT * FROM database.employee
+LIMIT 0, 1000
+
+-- Date: 2022-06-09 21:30
+*/
+INSERT INTO `` (`ID_Employee`,`Post`,`FIO_Employee`,`Login`,`Password`,`Last_Login`,`Status_LogIn`) VALUES ('ID 101','Продавец','Иванов Иван Иванович','Ivanov@namecomp.ru','2L6KZG','15:05:2022 13:13:00','Успешно');
+INSERT INTO `` (`ID_Employee`,`Post`,`FIO_Employee`,`Login`,`Password`,`Last_Login`,`Status_LogIn`) VALUES ('ID 102','Продавец','Петров Петр Петрович','petrov@namecomp.ru','uzWC67','15:05:2022 13:13:00','Успешно');
+INSERT INTO `` (`ID_Employee`,`Post`,`FIO_Employee`,`Login`,`Password`,`Last_Login`,`Status_LogIn`) VALUES ('ID 103','Администратор','Федоров Федор Федорович','fedorov@namecomp.ru','8ntwUp','15:05:2022 13:13:00','Успешно');
+INSERT INTO `` (`ID_Employee`,`Post`,`FIO_Employee`,`Login`,`Password`,`Last_Login`,`Status_LogIn`) VALUES ('ID 104','Старший смены','Миронов Вениамин Куприянович','mironov@namecomp.ru','YOyhfR','15:05:2022 13:13:00','Успешно');
+INSERT INTO `` (`ID_Employee`,`Post`,`FIO_Employee`,`Login`,`Password`,`Last_Login`,`Status_LogIn`) VALUES ('ID 105','Старший смены','Ширяев Ермолай Вениаминович','shiryev@namecomp.ru','RSbvHv','15:05:2022 13:13:00','Неуспешно');
+INSERT INTO `` (`ID_Employee`,`Post`,`FIO_Employee`,`Login`,`Password`,`Last_Login`,`Status_LogIn`) VALUES ('ID 106','Старший смены','Игнатов Кассиан Васильевич','ignatov@namecomp.ru','rwVDh9','15:05:2022 13:13:00','Успешно');
+INSERT INTO `` (`ID_Employee`,`Post`,`FIO_Employee`,`Login`,`Password`,`Last_Login`,`Status_LogIn`) VALUES ('ID 107','Продавец','Хохлов Владимир Мэлсович','hohlov@namecomp.ru','LdNyos','15:05:2022 13:13:00','Успешно');
+INSERT INTO `` (`ID_Employee`,`Post`,`FIO_Employee`,`Login`,`Password`,`Last_Login`,`Status_LogIn`) VALUES ('ID 108','Продавец','Стрелков Мстислав Георгьевич','strelkov@namecomp.ru','gynQMT','15:05:2022 13:13:00','Неуспешно');
+INSERT INTO `` (`ID_Employee`,`Post`,`FIO_Employee`,`Login`,`Password`,`Last_Login`,`Status_LogIn`) VALUES ('ID 109','Продавец','Беляева Лилия Наумовна','belyeva@@namecomp.ru','AtnDjr','15:05:2022 13:13:00','Успешно');
+INSERT INTO `` (`ID_Employee`,`Post`,`FIO_Employee`,`Login`,`Password`,`Last_Login`,`Status_LogIn`) VALUES ('ID 110','Продавец','Смирнова Ульяна Гордеевна','smirnova@@namecomp.ru','JlFRCZ','15:05:2022 13:13:00','Успешно');
